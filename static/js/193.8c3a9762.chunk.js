0be5fe4b731774550e3e604996d722abbe0fc1fd
Ivano4ka8 <120514890+Ivano4ka8@users.noreply.github.com>
2023-10-29 18:19:48 +0000

    Deploying to gh-pages from @ Ivano4ka8/goit-react-hw-08-phonebook@23dfc3b0aef385745882aa86996036c227676772 🚀

---
+++ static/js/193.8c3a9762.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[193],{9193:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var r=t(1087),c=t(1578),i=t(184);function o(){return(0,i.jsxs)("div",{style:{textAlign:"center"},children:[(0,i.jsx)(c.hlP,{size:"80px"}),(0,i.jsx)("p",{children:(0,i.jsx)(r.rU,{to:"/",children:"GO TO HOME "})})]})}}}]);
+//# sourceMappingURL=193.8c3a9762.chunk.js.map

a6a85c97c78316af8f91dd3ee41b73f5524a0689
Ivano4ka8 <ivannamihalova@gmail.com>
2023-10-30 12:48:08 +0200

    delete import

--- src/components/ContactsList/ContactList.jsx
+++ src/components/ContactsList/ContactList.jsx
@@ -1,13 +1,10 @@
 import { ContactItem } from 'components/ContactsItem/ContactsItem';
 import { ContactList } from './ContactList.styled';
 import { selectVisibleContacts } from 'redux/contacts/selectors';
-import { fetchContacts } from 'redux/contacts/contactsOperations';
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 export const ContactsList = () => {
   const visibleContacts = useSelector(selectVisibleContacts);
-  const dispatch = useDispatch();
 
   return (
     <ContactList>
